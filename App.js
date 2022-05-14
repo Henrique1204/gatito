2cbf002f416b33440d0b5ded85ba46d8e9dee6b8
Paulo <pauloh16.pdr@gmail.com>
2022-05-14 17:51:24 -0300

    Adicionar reactotron no projeto

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Reactotron from 'reactotron-react-native';
 
 import TelaPadrao from './src/componentes/TelaPadrao';
 import Rotas from './src/Rotas';
 
+Reactotron.configure().useReactNative().connect();
+console.tron = Reactotron;
+
 const App = () => {
+  console.tron.log('Op');
+
   return (
     <TelaPadrao>
       <Rotas />
